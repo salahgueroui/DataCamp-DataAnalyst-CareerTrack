9b0d0c10f9ed0bed63f5a9a7b473a62298cab882
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-13 23:57:38 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Exercise.sql
@@ -0,0 +1,17 @@
+-- Exercise
+-- Querying the books table
+
+-- Instructions 1
+-- Select the title from the books table to view all book titles.
+-- MY SOLUTION:
+
+SELECT title
+FROM books;
+
+-- Instructions 2
+-- Select both title and author from the books table.
+-- MY SOLUTION:
+
+-- Instructions 3
+-- Select all columns from the books table.
+-- MY SOLUTION:
