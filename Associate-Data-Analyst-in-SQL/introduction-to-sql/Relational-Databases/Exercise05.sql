eb992782179c59f32323c8b92d99a9e61e39a9b4
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-14 22:41:36 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/introduction-to-sql/Relational-Databases/Exercise05.sql
@@ -0,0 +1,11 @@
+-- Exercise
+-- Limiting results
+
+-- Instructions
+-- Using PostgreSQL, select the genre field from the books table; limit the number of results to 10.
+
+-- MY SOLUTION:
+
+Select genre
+from books
+limit 10;
