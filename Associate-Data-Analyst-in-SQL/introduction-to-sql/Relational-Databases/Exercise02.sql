f665e6c6ba8ce0b956381dd6a66e23bd6d57fe79
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-14 17:14:37 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/introduction-to-sql/Relational-Databases/Exercise02.sql
@@ -0,0 +1,14 @@
+-- Exercise
+-- Making queries DISTINCT
+
+-- Instructions 1
+-- Select only the unique authors from the books table.
+
+-- MY SOLUTION:
+
+SELECT DISTINCT(author)
+FROM books;
+
+-- Instructions 2
+
+-- MY SOLUTION:
