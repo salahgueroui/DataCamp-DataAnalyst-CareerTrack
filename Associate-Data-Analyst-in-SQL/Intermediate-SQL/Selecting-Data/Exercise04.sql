e6b89db3724860bb03418d15fe9e58952fc2d25a
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-17 23:14:34 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Selecting-Data/Exercise04.sql
@@ -0,0 +1,12 @@
+-- Exercise
+-- Order of execution
+
+-- Instructions
+-- Drop the SQL keywords into the order that they will be executed in (not the written order) from first (top) to last (bottom).
+limit
+from
+select
+-- MY SOLUTION:
+from
+select
+limit
