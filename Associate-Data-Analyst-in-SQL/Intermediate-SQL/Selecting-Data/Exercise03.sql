d9f19b0bc770b3d4e11f5f6bb617ff6f7c031200
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-17 21:47:14 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Selecting-Data/Exercise03.sql
@@ -0,0 +1,16 @@
+-- Exercise
+-- SELECT DISTINCT
+
+-- Instructions 1
+-- Return the unique countries represented in the films table using DISTINCT.
+
+-- MY SOLUTION:
+
+select distinct(country)
+from films;
+
+-- Instructions 2
+-- Return the number of unique countries represented in the films table, aliased as count_distinct_countries.
+
+
+-- MY SOLUTION:
