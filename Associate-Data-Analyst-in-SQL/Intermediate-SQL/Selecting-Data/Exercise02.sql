27143248cd7b6a82bd9a5275f23473ce4e7572d4
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-17 21:30:22 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Selecting-Data/Exercise02.sql
@@ -0,0 +1,23 @@
+-- Exercise
+-- Practice with COUNT()
+
+-- Instructions 1
+-- Count the total number of records in the people table, aliasing the result as count_records.
+
+-- MY SOLUTION:
+
+select count(id) as count_records
+from people;
+
+-- Instructions 2
+-- Count the number of records with a birthdate in the people table, aliasing the result as count_birthdate.
+
+-- MY SOLUTION:
+
+
+
+-- Instructions 3
+-- Count the records for languages and countries in the films table; alias as count_languages and count_countries.
+
+-- MY SOLUTION:
+
