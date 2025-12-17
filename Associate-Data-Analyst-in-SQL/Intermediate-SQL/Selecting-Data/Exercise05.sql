333419027294598e47e333a90c59abf9069b1fd8
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-17 23:25:24 +0100

    Add SQL solution

--- Associate-Data-Analyst-in-SQL/Intermediate-SQL/Selecting-Data/Exercise05.sql
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Selecting-Data/Exercise05.sql
@@ -15,8 +15,14 @@ LIMIT 5;
 
 -- Instructions 2
 -- Find the two errors in this code; the same error has been repeated twice.
+-- Debug this code
+SELECT film_id imdb_score num_votes
+FROM reviews;
 
 -- MY SOLUTION:
+
+SELECT film_id, imdb_score, num_votes
+FROM reviews;
 
 -- Instructions 3
 -- Find the two bugs in this final query.
