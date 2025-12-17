b2a5b39a7983f26e211ea989a4864dbd0055630d
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-17 21:20:20 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Selecting-Data/Exercise01.sql
@@ -0,0 +1,18 @@
+-- Exercise
+-- Learning to COUNT()
+
+-- Instructions
+-- Here is a query counting film_id. Select the answer below that correctly describes what the query will return.
+
+--SELECT COUNT(film_id) AS count_film_id
+--FROM reviews;
+
+-- Possible answers:
+-- 1. The number of unique films in the reviews table.
+-- 2. The number of records containing a film_id.
+-- 3. The total number of records in the reviews table.
+-- 4. The sum of the film_id field.
+
+-- MY SOLUTION:
+
+ 2. The number of records containing a film_id.
