f433b467fdd8ed5ff4cd0bbb08726bad78d675b8
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-18 01:09:50 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Selecting-Data/Exercise06.sql
@@ -0,0 +1,13 @@
+-- Exercise
+-- Formatting
+
+-- Instructions
+-- Adjust the sample code so that it is in line with standard practices.
+-- Rewrite this query
+select person_id, role from roles limit 10
+-- MY SOLUTION:
+
+SELECT person_id,
+       role
+FROM roles
+LIMIT 10;
