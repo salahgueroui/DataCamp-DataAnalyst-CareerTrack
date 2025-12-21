ab6a879e7963ac7458a2afa8d88c94299f69a48e
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-21 14:56:29 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Filtering-Records/Exercise02.sql
@@ -0,0 +1,13 @@
+-- Exercise
+-- Using WHERE with text
+
+
+-- Instructions
+-- Select and count the language field using the alias count_spanish.
+-- Apply a filter to select only Spanish from the language field.
+
+-- MY SOLUTION:
+
+select Count(language)as count_spanish
+from films
+where language='Spanish';
