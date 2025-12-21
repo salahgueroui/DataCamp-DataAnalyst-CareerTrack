4f846fead2c5ed65047da8f9558025d16e38f784
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-21 14:20:51 +0100

    Add SQL solution

--- Associate-Data-Analyst-in-SQL/Intermediate-SQL/Filtering-Records/Exercise01.sql
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Filtering-Records/Exercise01.sql
@@ -1,1 +1,23 @@
---salah
+-- Exercise
+-- Using WHERE with numbers
+
+
+-- Instructions 1
+-- Select the film_id and imdb_score from the reviews table
+-- and filter on scores higher than 7.0.
+
+-- MY SOLUTION:
+
+
+-- Instructions 2
+-- Select the film_id and facebook_likes of the first ten records
+-- with less than 1000 likes from the reviews table.
+
+-- MY SOLUTION:
+
+
+-- Instructions 3
+-- Count how many records have a num_votes of at least 100,000;
+-- use the alias films_over_100K_votes.
+
+-- MY SOLUTION:
