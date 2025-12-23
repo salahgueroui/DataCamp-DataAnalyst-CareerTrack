e3c2c2ca524823a555e7af64a6223239b48b0791
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-24 00:10:49 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Filtering-Records/Exercise03.sql
@@ -0,0 +1,27 @@
+-- Exercise
+-- Using AND
+
+
+-- Instructions 1
+-- Select the title and release_year for all German-language films
+-- released before 2000.
+
+-- MY SOLUTION:
+
+select title,release_year
+from films
+where language = 'German'
+    and release_year < 2000 ;
+
+-- Instructions 2
+-- Update the query from the previous step to show German-language films
+-- released after 2000 rather than before.
+
+-- MY SOLUTION:
+
+
+-- Instructions 3
+-- Select all details for German-language films released after 2000
+-- but before 2010 using only WHERE and AND.
+
+-- MY SOLUTION:
