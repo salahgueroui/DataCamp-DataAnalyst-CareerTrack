c5af70a108db4a658324b6d7aca754ff8575aabd
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-25 10:56:03 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Filtering-Records/Exercise05.sql
@@ -0,0 +1,31 @@
+-- Exercise
+-- Using BETWEEN
+
+
+-- Instructions 1
+-- Select the title and release_year of all films released between
+-- 1990 and 2000 (inclusive) using BETWEEN.
+
+-- MY SOLUTION:
+
+select title,release_year
+from films
+where release_year between 1990 and 2000
+
+-- Instructions 2
+-- Update the query to include only Spanish-language films.
+
+-- MY SOLUTION:
+
+
+-- Instructions 3
+-- Update the query to include only films with budgets over $100 million.
+
+-- MY SOLUTION:
+
+
+-- Instructions 4
+-- Combine all filters to get Spanish-language films released between
+-- 1990 and 2000 (inclusive) with budgets over $100 million.
+
+-- MY SOLUTION:
