6a13fc8d5b33bd716d042dd401058be5cabd4ab8
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-25 11:10:11 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Filtering-Records/LIKE-and-NOT LIKE.sql
@@ -0,0 +1,20 @@
+-- Exercise
+-- LIKE and NOT LIKE
+
+
+-- Instructions 1
+-- Select the names of all people whose names begin with 'B'.
+
+-- MY SOLUTION:
+
+
+-- Instructions 2
+-- Select the names of people whose names have 'r' as the second letter.
+
+-- MY SOLUTION:
+
+
+-- Instructions 3
+-- Select the names of people whose names don't start with 'A'.
+
+-- MY SOLUTION:
