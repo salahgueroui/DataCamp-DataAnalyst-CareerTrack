90a6e781708fe693a257ccd05d0a831a08dc40e1
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-12-25 11:01:31 +0100

    Add SQL solution

--- Associate-Data-Analyst-in-SQL/Intermediate-SQL/Filtering-Records/Exercise05.sql
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Filtering-Records/Exercise05.sql
@@ -41,3 +41,10 @@ and language='Spanish';
 -- 1990 and 2000 (inclusive) with budgets over $100 million.
 
 -- MY SOLUTION:
+
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+	AND budget > 100000000
+-- Amend the query to include Spanish or French-language films
+	AND (language='Spanish' or language='French');
