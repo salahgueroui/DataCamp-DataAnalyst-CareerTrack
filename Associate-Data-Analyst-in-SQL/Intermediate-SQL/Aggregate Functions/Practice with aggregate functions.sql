9ad0ba2671e20b8eeab8861d96da74fc64b7b2b0
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-09 16:22:19 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Aggregate Functions/Practice with aggregate functions.sql
@@ -0,0 +1,28 @@
+-- Exercise
+-- Practice with aggregate functions
+
+
+-- Instructions 1
+-- Use the SUM() function to calculate the total duration of all films
+-- and alias with total_duration.
+
+-- MY SOLUTION:
+
+
+-- Instructions 2
+-- Calculate the average duration of all films and alias with average_duration.
+
+-- MY SOLUTION:
+
+
+-- Instructions 3
+-- Find the most recent release_year in the films table,
+-- aliasing as latest_year.
+
+-- MY SOLUTION:
+
+
+-- Instructions 4
+-- Find the duration of the shortest film and use the alias shortest_film.
+
+-- MY SOLUTION:
