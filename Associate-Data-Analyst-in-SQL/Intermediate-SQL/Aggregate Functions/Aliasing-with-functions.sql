e0e89139e0167130eb8a5955ce87811c30c3bc29
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-14 00:42:02 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Aggregate Functions/Aliasing-with-functions.sql
@@ -0,0 +1,23 @@
+-- Exercise
+-- Aliasing with functions
+
+
+-- Instructions 1
+-- Select the title and duration in hours for all films and alias as duration_hours.
+-- Since the current durations are in minutes, you'll need to divide duration by 60.0.
+
+-- MY SOLUTION:
+
+
+-- Instructions 2
+-- Calculate the percentage of people who are no longer alive
+-- and alias the result as percentage_dead.
+
+-- MY SOLUTION:
+
+
+-- Instructions 3
+-- Find how many decades (period of ten years) the films table covers
+-- by using MIN() and MAX(); alias as number_of_decades.
+
+-- MY SOLUTION:
