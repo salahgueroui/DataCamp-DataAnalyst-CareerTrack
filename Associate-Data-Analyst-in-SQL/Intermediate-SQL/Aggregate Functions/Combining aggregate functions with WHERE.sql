8e122adddf1a9919348599c84cb8dc0567c0b94e
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-13 23:26:21 +0100

    Add SQL solution

--- Associate-Data-Analyst-in-SQL/Intermediate-SQL/Aggregate Functions/Combining aggregate functions with WHERE.sql
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Aggregate Functions/Combining aggregate functions with WHERE.sql
@@ -8,6 +8,9 @@
 
 -- MY SOLUTION:
 
+select sum(gross)as total_gross
+from films
+where release_year>=2000
 
 -- Instructions 2
 -- Calculate the average amount grossed by all films whose titles
@@ -15,15 +18,25 @@
 
 -- MY SOLUTION:
 
+select avg(gross)as avg_gross_A
+from films
+where title like 'A%'
 
 -- Instructions 3
 -- Calculate the lowest gross film in 1994 and use the alias lowest_gross.
 
 -- MY SOLUTION:
 
+select min (gross)as lowest_gross
+from films
+where release_year = 1994
 
 -- Instructions 4
 -- Calculate the highest gross film between 2000 and 2012, inclusive,
 -- and use the alias highest_gross.
 
 -- MY SOLUTION:
+
+select max (gross)as highest_gross
+from films
+where release_year between 2000 and 2012
