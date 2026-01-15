bf5085dfbc170cff178306ede793c866a3421cc9
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-15 22:06:03 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Sorting and Grouping/Sorting-single-fields.sql
@@ -0,0 +1,17 @@
+-- Exercise
+-- Sorting single fields
+
+
+-- Instructions 1
+-- Select the name of each person in the people table,
+-- sorted alphabetically.
+
+-- MY SOLUTION:
+
+
+
+-- Instructions 2
+-- Select the title and duration for every film,
+-- from longest duration to shortest.
+
+-- MY SOLUTION:
