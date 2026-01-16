2ab0c02dd02f20a6b5875d8038cd2f187d9c9f92
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-16 02:03:52 +0100

    Add SQL solution

--- Associate-Data-Analyst-in-SQL/Intermediate-SQL/Sorting and Grouping/Filter-with-HAVING.sql
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Sorting and Grouping/Filter-with-HAVING.sql
@@ -9,3 +9,11 @@
 -- Filter results to only include counts greater than 10
 
 -- MY SOLUTION:
+
+-- Select the country and distinct count of certification as certification_count
+Select country,count (distinct certification) as certification_count
+from films
+-- Group by country
+group by country
+-- Filter results to countries with more than 10 different certifications
+having count(distinct certification)>10
