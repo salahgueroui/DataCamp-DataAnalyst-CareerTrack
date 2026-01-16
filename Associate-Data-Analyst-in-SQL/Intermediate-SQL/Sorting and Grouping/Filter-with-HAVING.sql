15681e8afa6bc346857da80f0b5e6ce4ee535dfb
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-01-16 02:03:29 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Intermediate-SQL/Sorting and Grouping/Filter-with-HAVING.sql
@@ -0,0 +1,11 @@
+-- Exercise
+-- Filter with HAVING
+
+-- Instructions
+-- Select country from the films table
+-- Get the distinct count of certification
+-- Alias the count as certification_count
+-- Group the results by country
+-- Filter results to only include counts greater than 10
+
+-- MY SOLUTION:
